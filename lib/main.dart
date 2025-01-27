46ed4dc6cccd0a7b5b7813e9e3c6523e75480832
haresh <haresh.v521@gmail.com>
2025-01-27 15:29:21 +0530

    Upgrade with latest

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,25 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+
 import 'home/home_screen.dart';
- void main() async {
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyCdS69M0pNQBMPDQPXW08LNJqhunWOBO10",
+          authDomain: "society-parking.firebaseapp.com",
+          projectId: "society-parking",
+          storageBucket: "society-parking.appspot.com",
+          messagingSenderId: "372126564712",
+          appId: "1:372126564712:web:fb1fc9ee36b15649ea96dd",
+          measurementId: "G-675JKPM6VN"));
   runApp(GetMaterialApp(
-   routes: {
-    'home': (context) => const HomeScreen(),
-   },
-   debugShowCheckedModeBanner: false,
+    routes: {
+      'home': (context) => const HomeScreen(),
+    },
+    debugShowCheckedModeBanner: false,
     home: const HomeScreen(),
   ));
 }
-
